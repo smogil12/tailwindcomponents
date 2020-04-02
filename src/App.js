c3dd838c7d094aa77efa8e174f157245d60004db
Spencer Mogil <smogil12@gmail.com>
2020-04-01 22:20:00 -0400

    tailwind css working need to practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { FillButton } from "tailwind-react-ui";
-import "./App.css";
-
-export default function App() {
-  const MyButton = () => <FillButton brand="primary">Submit</FillButton>;
+function App() {
   return (
-    <div className="App">
-      <h1>Tailwind Practice</h1>
-      <MyButton />
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img
+        className="w-full"
+        src="https://townsquare.media/site/366/files/2018/10/metallica_by_Ross_Halfin.jpg?w=980&q=75"
+        alt="Display"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Blessing Krofegha
+        </div>
+        <p className="text-gray-700 text-base">
+          When i’m not coding i switch to netflix with biscuits and cold tea as
+          my companion. <span></span>😜
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Software Engineer
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Writter
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">
+          #Public Speaker
+        </span>
+      </div>
     </div>
   );
 }
+export default App;
